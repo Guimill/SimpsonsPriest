93d1fe443948a286f56d9b42ca3971f5bd209c99
Guimill <86968380+Guimill@users.noreply.github.com>
2023-07-07 00:05:50 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header></header>
+      <div className='body'>
+       <i style={{fontSize:"6rem"}}>Welcome</i> <br/><span style={{fontSize:"2rem"}}>to the</span><br/>
+        <b style={{color:"rgba(1,15,108,255)", textShadow:"1px 1px 0px black", fontSize:"9rem"}}> <i>e</i>Piscopale <br/> church</b>
+      </div>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
