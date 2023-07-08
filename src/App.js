68d8fe6a6ae684adacd5f301937436bee1b6d034
Guimill <86968380+Guimill@users.noreply.github.com>
2023-07-08 18:23:56 +0200

    first frame over

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import './App.css';
 import bell from "./bell.png"
 import shield from "./shield.png"
-import main from "./main.png"
+import main1 from "./main1.png"
+import main2 from "./main2.png"
+
 
 function App() {
   return (
     <div className="App">
       <header></header>
+      <img style={{top:"16vh", left:"85vw", position:"absolute", zIndex:"2", scale:"1.5"}} src={bell} alt='bells' />
+      <img style={{top:"12vh", left:"8vw", position:"absolute", zIndex:"1", scale:"1.2"}} src={shield} alt='shield' />
+      <img style={{top:"78vh", left:"10vw", position:"absolute", zIndex:"1", scale:"1.5"}} src={main1} alt='main1' />
+      <img style={{top:"78vh", left:"90vw", position:"absolute", zIndex:"1", scale:"1.5"}} src={main2} alt='main2' />
       <div className='body'>
-        <img style={{top:"60px", left:"150px", position:"relative"}} src={bell} alt='bells' />
-        <img src={shield} alt='bells' />
-        <img src={main} alt='bells' />
-        <img src={main} alt='bells' />
         <p className='welcome'>Welcome</p>
         <p>to the</p>
-        <h1><i>e</i>Piscopale <br /> church</h1>
+        <h1><i>e</i>PISCOPAL <br /> CHURCH</h1>
       </div>
       <footer></footer>
     </div>
