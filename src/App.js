559d4e6066e2e05a483921b2fcc84b40c857f556
Guimill <guillaume.milleret@gmail.com>
2023-07-08 15:58:32 +0200

    add image

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
-import bell from "bell.png"
+import bell from "./bell.png"
+import shield from "./shield.png"
+import main from "./main.png"
 
 function App() {
   return (
     <div className="App">
-      <img src={bell}></img>
       <header></header>
       <div className='body'>
+        <img style={{top:"60px", left:"150px", position:"relative"}} src={bell} alt='bells' />
+        <img src={shield} alt='bells' />
+        <img src={main} alt='bells' />
+        <img src={main} alt='bells' />
         <p className='welcome'>Welcome</p>
         <p>to the</p>
-        <h1><i>e</i>Piscopale <br/> church</h1>
+        <h1><i>e</i>Piscopale <br /> church</h1>
       </div>
       <footer></footer>
     </div>
