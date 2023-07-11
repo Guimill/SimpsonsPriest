6f6b0afe5ba976281e592588eddf82abc5ef2064
Guimill <guillaume.milleret@gmail.com>
2023-07-11 21:45:08 +0200

    Update Formulaire.jsx

--- src/Formulaire.jsx
+++ src/Formulaire.jsx
@@ -2,28 +2,47 @@ import './App.css';
 import dove1 from './dove1.png'
 import dove2 from './dove2.png'
 import church from './eglise.png'
-import collerette from './collerette.png'
 import { jsPDF } from "jspdf";
+import { useState } from 'react';
+import collerette from './collerette.png'
+
 
 const doc = new jsPDF();
+doc.addImage(collerette, 'PNG', 0,0,0,0)
+
 
 function Formulaire() {
+
+    const [Name, setName] = useState("");
+    const [submitted, setSubmitted] = useState('');
+
+    function handleChange(e) {
+        setName(e.target.value);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        doc.text(75,10, Name);
+        setSubmitted(doc.save(Name.replace(/\s/g, '')));
+    }
+
+
+
     return (
-      <div>
-        <img style={{top:"10vh", left:"85vw", position:"absolute", scale:"2"}} src={dove2} alt='dove2' />
-        <img style={{top:"10vh", left:"8vw", position:"absolute", scale:"2"}} src={dove1} alt='dove1' />
-        <img style={{top:"65vh", left:"10vw", position:"absolute", scale:"2"}} src={church} alt='church' />
-        <img style={{top:"65vh", left:"85vw", position:"absolute", scale:"2"}} src={church} alt='church' />
-        <div className='bodyFormulaire'>
-            <h1 style={{paddingTop:"22vh", fontSize:"9em"}}>INTERNET <br /> DIVINITY <br /> SCHOOL </h1>
-            <form action="" method="get">
-                <a href={collerette} download="collerette"><label style={{paddingTop:"8vh", fontSize:"4em"}} for="name"> NAME </label></a>
-                <input style={{fontSize:"3em", border:"solid"}} type="text" name="name" id="name" required></input>
-            </form>
+        <div>
+            <img style={{ top: "10vh", left: "85vw", position: "absolute", scale: "2" }} src={dove2} alt='dove2' />
+            <img style={{ top: "10vh", left: "8vw", position: "absolute", scale: "2" }} src={dove1} alt='dove1' />
+            <img style={{ top: "65vh", left: "10vw", position: "absolute", scale: "2" }} src={church} alt='church' />
+            <img style={{ top: "65vh", left: "85vw", position: "absolute", scale: "2" }} src={church} alt='church' />
+            <div className='bodyFormulaire'>
+                <h1 style={{ paddingTop: "22vh", fontSize: "9em" }}>INTERNET <br /> DIVINITY <br /> SCHOOL </h1>
+                <form onSubmit={handleSubmit}>
+                    <label style={{ paddingTop: "8vh", fontSize: "4em" }} for="name"> NAME </label>
+                    <input onChange={handleChange} style={{ fontSize: "3em", border: "solid" }} type="text" name="name" id="name" value={Name} required></input>
+                </form>
+            </div>
         </div>
-      </div>
     );
-  }
-  
-  export default Formulaire;
-  
+}
+
+export default Formulaire;
