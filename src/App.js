a374b7fc7b76d3527562e4b8ff23bb3157be4e96
Guimill <guillaume.milleret@gmail.com>
2023-07-12 15:15:46 +0200

    responsivness over !

--- src/App.js
+++ src/App.js
@@ -7,17 +7,17 @@ import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <div className='FirstPage'>
+      <img id='bells' style={{top:"16vh", left:"85vw", position:"absolute"}} src={bell} alt='bells' />
+      <img id='shield' style={{top:"12vh", left:"8vw", position:"absolute"}} src={shield} alt='shield' />
+      <img style={{top:"78vh", left:"10vw", position:"absolute"}} src={main1} alt='main1' />
+      <img style={{top:"78vh", left:"90vw", position:"absolute"}} src={main2} alt='main2' />
       <header></header>
-      <img style={{top:"16vh", left:"85vw", position:"absolute", zIndex:"2", scale:"1.5"}} src={bell} alt='bells' />
-      <img style={{top:"12vh", left:"8vw", position:"absolute", zIndex:"1", scale:"1.2"}} src={shield} alt='shield' />
-      <img style={{top:"78vh", left:"10vw", position:"absolute", zIndex:"1", scale:"1.5"}} src={main1} alt='main1' />
-      <img style={{top:"78vh", left:"90vw", position:"absolute", zIndex:"1", scale:"1.5"}} src={main2} alt='main2' />
       <div className='bodyApp'>
         <p className='welcome'>Welcome</p>
         <p>to the</p>
         <Link to={'./Formulaire'}>
-          <h1><i>e</i>PISCOPAL <br /> CHURCH</h1>
+          <h1 id='ePiscopal'><i>e</i>PISCOPAL <br /> CHURCH</h1>
         </Link>
       </div>
       <footer></footer>
