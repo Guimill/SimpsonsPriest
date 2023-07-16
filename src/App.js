e890d7a63ed7159bdfcb0756c43084023476b45a
Guimill <guillaume.milleret@gmail.com>
2023-07-16 22:08:21 +0200

    Add context audio

--- src/App.js
+++ src/App.js
@@ -4,10 +4,23 @@ import shield from "./shield.png"
 import main1 from "./main1.png"
 import main2 from "./main2.png"
 import { Link } from "react-router-dom";
+import soundfile from './into.mp3'
+
 
 function App() {
+
+  let audio = new Audio(soundfile)
+
+  function handleMouse(e) {
+      e.preventDefault();
+      audio.play();
+      }
+
+      alert("Montez le son SVP")
+
   return (
-    <div className='FirstPage'>
+    <div onMouseDown={handleMouse} className='FirstPage'>
+    <iframe title='audio' hidden src={soundfile} allow="autoplay"></iframe> 
       <img id='bells' style={{top:"16vh", left:"85vw", position:"absolute"}} src={bell} alt='bells' />
       <img id='shield' style={{top:"12vh", left:"8vw", position:"absolute"}} src={shield} alt='shield' />
       <img style={{top:"78vh", left:"10vw", position:"absolute"}} src={main1} alt='main1' />
